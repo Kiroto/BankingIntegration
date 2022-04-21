9694b6f4d661d3c039c5722a446096bc23f426c5
Jerry Rivas <kiroto50@gmail.com>
2022-04-21 12:16:14 -0400

    De-Ignored error message and user session classes

---
+++ BankingIntegration/BankModel/General/Responses/ErrorMesage.cs
@@ -0,0 +1,40 @@
+﻿using BankingIntegration.HTTP;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+
+namespace BankingIntegration.BankModel
+{
+    public enum ErrorCode : ushort
+    {
+        CORE_OFFLINE = 1,
+        CREDENTIALS_INVALID = 2,
+        KEY_INVALID = 3,
+    }
+    class ErrorMesage : BankSerializable, IResponsible
+    {   
+        [JsonPropertyName("ErrorCode")]
+        public ErrorCode Code { get; set; }
+
+        [JsonPropertyName("ErrorMessage")]
+        public string ErrorMessage { get; set; }
+
+        public string AsJsonString()
+        {
+            return JsonSerializer.Serialize(this);
+        }
+
+        public int StatusCode { get; set; }
+
+        public ProcessedResponse buildResponse()
+        {
+            return new ProcessedResponse()
+            {
+                Contents = AsJsonString(),
+                StatusCode = StatusCode
+            };
+        }
+    }
+}
---
+++ BankingIntegration/BankModel/General/Responses/UserSession.cs
@@ -0,0 +1,49 @@
+﻿using BankingIntegration.HTTP;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+
+namespace BankingIntegration.BankModel
+{
+    class UserSession : BankSerializable, IResponsible
+    {
+        // The user ID
+        [JsonPropertyName("UserId")]
+        public int UserID { get; set; }
+
+        // The session token for the user.
+        [JsonPropertyName("SessionToken")]
+        public string SessionToken { get; set; }
+
+        // When this token last made its request
+        public DateTime LastRequest;
+        
+        // When the session started
+        public DateTime SessionStart;
+
+        // The requesting service
+        public int Service;
+
+        public string AsJsonString()
+        {
+            return JsonSerializer.Serialize(this);
+        }
+
+        public void Refresh()
+        {
+            LastRequest = DateTime.Now;
+        }
+
+        public int StatusCode { get; set; }
+
+        public ProcessedResponse buildResponse()
+        {
+            return new ProcessedResponse() {
+                Contents = AsJsonString(),
+                StatusCode = StatusCode
+            };
+        }
+    }
+}
