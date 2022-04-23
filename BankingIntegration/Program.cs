f16c4bb851851ff9b6f636478fc3d60e85305776
Jerry Rivas <kiroto50@gmail.com>
2022-04-22 21:30:00 -0400

    Added Log4j

--- BankingIntegration/Program.cs
+++ BankingIntegration/Program.cs
@@ -6,30 +6,36 @@ namespace BankingIntegration
 {
     class Program
     {
-
+        private static readonly log4net.ILog logger
+               = log4net.LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
         static void Main(string[] args)
         {
             Console.WriteLine("Starting server");
             IntegrationServer server = new IntegrationServer();
+            logger.Info("HELLO THERE");
+            server.NewLog += HandleServerLog;
             server.Start();
             Console.WriteLine($"Listening at port {server.port}");
-            server.NewLog += HandleServerLog;
             Console.ReadKey();
             server.Stop();
         }
 
         static void HandleServerLog(Log log)
         {
+            string logtext = log.ToString();
             switch (log.Severity)
             {
                 case Log.LogSeverity.Info:
                     Console.ForegroundColor = ConsoleColor.Cyan;
+                    logger.Info(logtext);
                     break;
                 case Log.LogSeverity.Warning:
                     Console.ForegroundColor = ConsoleColor.Yellow;
+                    logger.Warn(logtext);
                     break;
                 case Log.LogSeverity.Error:
                     Console.ForegroundColor = ConsoleColor.Red;
+                    logger.Error(logtext);
                     break;
                 default:
                     break;
